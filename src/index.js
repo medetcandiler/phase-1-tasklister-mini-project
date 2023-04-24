bc23de4f523ef086b3fc11274432fffd8aa94e8e
Medetcan Diler <medetdiler1996@gmail.com>
2023-04-24 22:23:24 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+  const input = document.querySelector('#new-task-description');
+  const ul = document.querySelector('#tasks');
+
+  // form submit event
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    if (input.value === '') {
+      alert('write sth to add as a task');
+    } else {
+      newItem(input.value)
+    }
+    input.value = '';
+  });
+
+  // delete item event
+  ul.addEventListener('click', e => {
+    if( e.target.id === 'btn'){
+      let li = e.target.parentElement
+      li.remove();
+    }
+  })
+
+  function newItem(value) {
+    let newLi = document.createElement('li');
+    let btn = document.createElement('button');
+    newLi.textContent = value;
+    btn.innerHTML = 'x';
+    btn.style.marginLeft = '1rem'
+    btn.setAttribute('id', 'btn')
+    newLi.appendChild(btn);
+    ul.appendChild(newLi)
+  }
+
+  function deleteItem(){
+
+  }
 });
